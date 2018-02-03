7beef6e739dace0fa55430a1e8033304f443f736
Alex Page <alex@alexpage.com.au>
2018-02-03 13:35:52 +1100

    development environment set up

---
+++ README.md
@@ -0,0 +1,12 @@
+# Batcave
+
+> 🦇 Automated set up of ubuntu desktop for a development environment
+
+
+# Get started
+
+Once ubuntu desktop is installed open the terminal and run:
+
+```
+wget -O- https://raw.githubusercontent.com/alexpage/batcave/master/init.sh | sh
+```
---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Install git python-pip and python-dev
+sudo apt-get install -y git python-pip python-dev
+
+# Install ansible with pip
+sudo pip install ansible
+
+# Clone the repositry and go into the file
+git clone https://github.com/alexpage/batcave.git
+cd batcave
+
+# Run the ansible playbook
+ansible-playbook config.yml --sudo
