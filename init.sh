eb67e42c2c7e89ff01711bac5727d8b07fbb9701
Alex Page <alex@alexpage.com.au>
2018-02-03 19:56:06 +1100

    updating node, fixing submodule issue

--- init.sh
+++ init.sh
@@ -6,10 +6,11 @@ sudo apt-get install -y build-essential git aptitude python-pip python-dev
 # Install ansible with pip
 sudo -H pip install ansible
 
-# Clone the repositry and go into the file
+# Clone the repositry and the sub modules and go into the file
 git clone https://github.com/alex-page/batcave.git
-git submodule update --init --recursive
 cd batcave
+git submodule update --init --recursive
+
 
 # Run the ansible playbook
 ansible-playbook config.yml --ask-become-pass
