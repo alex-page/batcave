39bbea79e76a71bae21cc1a7d8005ca079fcabd1
Alex Page <alex@alexpage.com.au>
2018-02-03 19:10:34 +1100

    Adding submodules with new name

--- init.sh
+++ init.sh
@@ -7,7 +7,8 @@ sudo apt-get install -y build-essential git aptitude python-pip python-dev
 sudo -H pip install ansible
 
 # Clone the repositry and go into the file
-git clone --recursive https://github.com/alex-page/batcave.git
+git clone https://github.com/alex-page/batcave.git
+git submodule update --init --recursive
 cd batcave
 
 # Run the ansible playbook
