47db2e1c4f6d7ff2ce139545c6477044e43fe47c
Alex Page <alex@alexpage.com.au>
2018-02-20 23:31:38 +1100

    trying to merge vars

--- roles/ssh-key/tasks/addkey.sh
+++ roles/ssh-key/tasks/addkey.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
-spawn source /home/$1/.bashrc
+spawn source $1
 expect "Enter passphrase for ~/.ssh/id_rsa:"
-send "$2\n";
+send $2;
 expect "Identity added: ~/.ssh/id_rsa (~/.ssh/id_rsa)"
 interact
