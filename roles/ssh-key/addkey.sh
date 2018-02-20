4a882a74158887e63c89b13223b4a075fb3164ef
Alex Page <alex@alexpage.com.au>
2018-02-20 23:10:31 +1100

    try ssh-add

--- roles/ssh-key/addkey.sh
+++ roles/ssh-key/addkey.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn source ~/.bashrc
+spawn ssh-add ~/.ssh/id_rsa
 expect "Enter passphrase for ~/.ssh/id_rsa:"
 send "$KEY\n";
 expect "Identity added: ~/.ssh/id_rsa (~/.ssh/id_rsa)"
