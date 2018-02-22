13b35b1cade1b64decdac8374cb8cb0ff21cc342
Alex Page <alex@alexpage.com.au>
2018-02-22 20:04:31 +1100

    Trying different ssh key spawn

--- roles/ssh-key/tasks/addkey.sh
+++ roles/ssh-key/tasks/addkey.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn source $SOURCE_DIR
+spawn keychain --eval ssh id_rsa "$(ssh-agent -s)"
 expect "Enter passphrase for ~/.ssh/id_rsa:"
 send $KEY;
 expect "Identity added: ~/.ssh/id_rsa (~/.ssh/id_rsa)"
