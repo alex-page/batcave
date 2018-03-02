1145d356aae3f823245449a0f12986e19f396235
Alex Page <alex@alexpage.com.au>
2018-03-03 08:40:28 +1100

    Trying to fix race condition

--- init.sh
+++ init.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Update all the packages
-sudo apt-get update
+sudo apt-get update;
 
 # Install required packages
-sudo apt-get install -y build-essential git aptitude python-pip python-dev
+sudo apt-get install -y build-essential git aptitude python-pip python-dev;
 
 # Install ansible with pip
-sudo pip install ansible cryptography
+sudo pip install ansible cryptography;
 
 # Clone the repositry and the sub modules and go into the file
-git clone https://github.com/alex-page/batcave.git
-cd batcave
-git submodule update --init --recursive
+git clone https://github.com/alex-page/batcave.git;
+cd batcave;
+git submodule update --init --recursive;
 
 # Run the ansible playbook
-ansible-playbook config.yml --ask-become-pass --ask-vault-pass
+ansible-playbook config.yml --ask-become-pass --ask-vault-pass;
