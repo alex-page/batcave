18a54b12ca550a432e76c11ff850fa82050d00d0
Alex Page <alex@alexpage.com.au>
2018-05-19 22:01:57 +1000

    Add prompts to shell script

--- init.sh
+++ init.sh
@@ -12,5 +12,11 @@ git clone https://github.com/alex-page/batcave.git;
 cd batcave;
 git submodule update --init --recursive;
 
+# Prompt the user for their information
+read -p 'What is your github username?: ' github_username;
+read -sp 'What is your github personal access token ( GPG and SSH )?: ' github_token;
+read -sp 'Please enter a secure SSH private key password?: ' ssh_password;
+read -sp 'Please enter a secure GPG private key password?: ' gpg_password;
+
 # Start the ansible task
-ansible-playbook ./config.yml --ask-become-pass;
+ansible-playbook ./config.yml --ask-become-pass -e "github_username=$github_username github_token=$github_token ssh_password=$ssh_password gpg_password=$gpg_password";
